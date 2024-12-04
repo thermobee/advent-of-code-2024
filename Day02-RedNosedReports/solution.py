5c885398f9eca34b2a0e1f7dd958f8ea6fece9e2
Tod Lazarov <tod.lazarov@gmail.com>
2024-12-03 20:47:32 -0500

    Solution to the second part, will not work for the first. Check the previous commit for that

--- Day02-RedNosedReports/solution.py
+++ Day02-RedNosedReports/solution.py
@@ -15,20 +15,38 @@ safe_reports_counter = 0
 
 # Function to shuffle through the numbers and compare
 def compare_numbers(my_list):
-    for i in range(len(my_list) - 1):
-        a, b = my_list[i], my_list[i + 1]
-        if (abs(a - b) > 3) or (abs(a - b) < 1):
-            return(False)
-    return(True)
+    # Compare existing line to a sorted/reverse sorted to confirm ascending/descending order
+    if my_list == sorted(my_list, reverse=False) or my_list == sorted(my_list, reverse=True):
+        # Compare each pair of numbers
+        for i in range(len(my_list) - 1):
+            a, b = my_list[i], my_list[i + 1]
+            if (abs(a - b) > 3) or (abs(a - b) < 1):
+                return(False)
+        return(True)
+
+    return(False)
+
+# Function that creates a new list and removes the given index
+def check_tolerance(my_list, index):
+    list_copy = my_list[:]
+    list_copy.pop(index)
+
+    return(list_copy)
 
 # Brake down the problem to individual lists
 for line in file:
     reports = [int(i) for i in line.split(' ')]
 
-    # Compare existing line to a sorted/reverse sorted to confirm ascending/descending
-    if reports == sorted(reports, reverse=False) or reports == sorted(reports, reverse=True) :
-        answer = compare_numbers(reports)
-        if answer == True:
-            safe_reports_counter += 1
+    index = 0
+
+    if compare_numbers(reports) == True:
+        safe_reports_counter +=1
+    else:
+        for i in range(len(reports)):
+            tolerated_report = check_tolerance(reports, i)
+            if compare_numbers(tolerated_report):
+                index += 1
+        if index >= 1:
+            safe_reports_counter +=1
 
 print(safe_reports_counter)
